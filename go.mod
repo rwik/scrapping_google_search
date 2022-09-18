8c9281af8fce121e46d84e6a3522f97933c6cf76
Rwik Mukhopadhyay <rwikee@gmail.com>
2022-09-18 13:41:16 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/rwik/scrapping_google_search
+
+go 1.18
+
+require (
+	github.com/gorilla/csrf v1.7.1
+	github.com/gorilla/mux v1.8.0
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gorilla/csrf"
+	"github.com/gorilla/mux"
+)
+
+var googleURLS = map[string]string{
+	"com": "https://www.google.com/search?q=",
+	"bn":  "https://www.google.com.bn/search?q=",
+	"in":  "https://www.google.co.in/search?q=",
+}
+
+type SearchResult struct {
+	rank   int
+	url    string
+	title  string
+	result string
+}
+
+var userAgents = []string{
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+	"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+}
+
+func getUAString() string {
+
+	return userAgents[0]
+}
+
+func buildGoogleUrls(searchTerm, countryCode, languageCode string, pages, count int) ([]string, error) {
+	toScrape := []string{}
+	searchTerm = strings.Trim(searchTerm, " ")
+	searchTerm = strings.Replace(searchTerm, " ", "+", -1)
+	if googleBase, found := googleURLS[countryCode]; found {
+		for i := 0; i < pages; i++ {
+			start := i * count
+			scrapeURL := fmt.Sprintf("%s%s&num=%d&hl=%s&start=%d&filter=0", googleBase, searchTerm, count, languageCode, start)
+			toScrape = append(toScrape, scrapeURL)
+		}
+	} else {
+		err := fmt.Errorf("country (%s) is currently not supported", countryCode)
+		return nil, err
+	}
+	return toScrape, nil
+
+}
+
+func googleResultParsing(response *http.Response, rank int) ([]SearchResult, error) {
+	doc, err := goquery.NewDocumentFromResponse(response)
+
+	if err != nil {
+		return nil, err
+	}
+
+	results := []SearchResult{}
+	sel := doc.Find("div.g")
+	rank++
+	for i := range sel.Nodes {
+		item := sel.Eq(i)
+		linkTag := item.Find("a")
+		link, _ := linkTag.Attr("href")
+		titleTag := item.Find("h3.r")
+		descTag := item.Find("span.st")
+		desc := descTag.Text()
+		title := titleTag.Text()
+		link = strings.Trim(link, " ")
+
+		if link != "" && link != "#" && !strings.HasPrefix(link, "/") {
+			result := SearchResult{
+				rank,
+				link,
+				title,
+				desc,
+			}
+			results = append(results, result)
+			rank++
+		}
+	}
+	return results, err
+
+}
+
+func getScrapeClient(proxyString interface{}) *http.Client {
+
+	switch v := proxyString.(type) {
+
+	case string:
+		proxyUrl, _ := url.Parse(v)
+		return &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+	default:
+		return &http.Client{}
+	}
+}
+
+func gglQuery(searchTerm, countryCode, languageCode string, proxyString interface{}, pages, count, backoff int) ([]SearchResult, error) {
+	results := []SearchResult{}
+	resultCounter := 0
+	googlePages, err := buildGoogleUrls(searchTerm, countryCode, languageCode, pages, count)
+	if err != nil {
+		return nil, err
+	}
+	for _, page := range googlePages {
+		res, err := scrapeClientRequest(page, proxyString)
+		if err != nil {
+			return nil, err
+		}
+		data, err := googleResultParsing(res, resultCounter)
+		if err != nil {
+			return nil, err
+		}
+		resultCounter += len(data)
+		for _, result := range data {
+			results = append(results, result)
+		}
+		time.Sleep(time.Duration(backoff) * time.Second)
+	}
+	return results, nil
+}
+
+func scrapeClientRequest(searchURL string, proxyString interface{}) (*http.Response, error) {
+	baseClient := getScrapeClient(proxyString)
+	req, _ := http.NewRequest("GET", searchURL, nil)
+	req.Header.Set("User-Agent", getUAString())
+
+	res, err := baseClient.Do(req)
+	if res.StatusCode != 200 {
+		err := fmt.Errorf("scraper received a non-200 status code suggesting a ban")
+		return nil, err
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", nil)
+	http.Handle("/", csrf.Protect([]byte(nil))(r))
+	var queryString string
+	fmt.Println("Please enter search string ")
+	fmt.Scanln(&queryString)
+	res, err := gglQuery(queryString, "in", "en", nil, 1, 30, 10)
+	var colorFlag bool
+	yellow := "\033[33m"
+	blue := "\033[34m"
+	if err == nil {
+		for _, res := range res {
+			if colorFlag {
+				colorFlag = false
+				fmt.Println(blue, res, "\033[0m")
+
+			} else {
+				colorFlag = true
+				fmt.Println(yellow, res, "\033[0m")
+
+			}
+
+		}
+	}
+}
